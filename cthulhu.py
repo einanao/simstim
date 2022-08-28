8672a5393cb829a0a265b76ace919fd5fd9481d2
einanao <nanaoei@protonmail.com>
2022-08-28 19:43:16 -0400

    fix missing init stim

--- cthulhu.py
+++ cthulhu.py
@@ -1,4 +1,6 @@
 import serial
+import time
+
 
 class CthulhuShield(object):
 
@@ -12,11 +14,12 @@ class CthulhuShield(object):
          1,  2
   ]
 
-  def __init__(self, port='/dev/tty.usbmodem114401', debug_mode=False):
+  def __init__(self, port='/dev/tty.usbmodem114401', debug_mode=False, conn_delay=1):
     self.debug_mode = debug_mode
     if not self.debug_mode:
       self.ard = serial.Serial(port, 9600, timeout=5)
       self.ard.flush()
+      time.sleep(conn_delay)
 
   def stim(self, idxes):
     if not self.debug_mode:
